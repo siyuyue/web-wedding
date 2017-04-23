62b136b841e77633cd73b9dbdf05a6e393e149fd
siyuyue <ysysamuel@gmail.com>
2017-04-23 14:41:08 -0700

    Use bootstrap, more pages, rsvp form (#6)
    * checkpoint work
    
    bootstrap, new nav bar, wedding day page
    
    * Checkpoint work 2
    
    More pages and RSVP form.
    
    * forget to add js
    
    * checkpoint work 3
    
    forms and js to manipulate form
    
    * checkpoint work 4
    
    modify form, cleanup

---
+++ wedding.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+	//Navigation
+	menu: '#menu',
+	anchors: ['welcome', 'weddingDay', 'gifts', 'rsvp'],
+	navigation: false,
+	
+	verticalCentered: true,
+	sectionsColor: ['', '#FFFFFF', '#FFFFF0', '#F5F5DC'],
+  });
+  
+  var adult_entry = $("#guests-adult-entry .entry")
+  var child_entry = $("#guests-child-entry .entry")
+  var adult_entry_header = $("#guests-adult-entry .entry-header")
+  var child_entry_header = $("#guests-child-entry .entry-header")
+  $("#guests-adult-entry").empty();
+  $("#guests-child-entry").empty();
+
+  $("#rsvp-form #guest-adult").change(function() {
+	  var adults_count = $("#guest-adult").val();
+	  $("#guests-adult-entry").empty();
+	  if (adults_count > 0) {
+		  $("#guests-adult-entry").append(adult_entry_header)
+	  }
+	  for (var i = 0; i < adults_count; i++) {
+		  $("#guests-adult-entry").append(adult_entry.clone());
+	  }  
+  })
+  
+  $("#rsvp-form #guest-child").change(function() {
+	  var children_count = $("#guest-child").val();
+	  $("#guests-child-entry").empty();
+	  if (children_count > 0) {
+		  $("#guests-child-entry").append(child_entry_header)
+	  }
+	  for (var i = 0; i < children_count; i++) {
+		  $("#guests-child-entry").append(child_entry.clone());
+	  }  
+  })
+});
